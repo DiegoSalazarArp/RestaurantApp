2f3d4e321fe95d6797a4915ea398b0f1943d815b
Diego Salazar Arp <d.salazararp@gmail.com>
2019-11-12 21:24:30 -0300

    Initial Commit

---
+++ RestaurantApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestaurantApp
+//
+//  Created by Diego Salazar Arp on 12-11-19.
+//  Copyright © 2019 Diego Salazar Arp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
